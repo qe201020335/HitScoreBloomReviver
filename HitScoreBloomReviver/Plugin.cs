df7b239e57c096f24d4599e7e36e6cb9650a0892
qe201020335 <qe201020335@sina.com>
2025-04-04 23:43:59 -0400

    Refactor Plugin.cs with new harmony id

--- HitScoreBloomReviver/Plugin.cs
+++ HitScoreBloomReviver/Plugin.cs
@@ -1,38 +1,36 @@
 ﻿using HarmonyLib;
 using IPA;
 using Logger = IPA.Logging.Logger;
-using System.Reflection;
 
 namespace HitScoreBloomReviver
 {
     [Plugin(RuntimeOptions.DynamicInit)]
     public class Plugin
     {
-        private static Harmony _harmony;
-        internal static Logger Logger { get; private set; }
-        internal static Plugin Instance { get; private set; }
+        private readonly Harmony _harmony = new ("com.github.qe201020335.HitScoreBloomReviver");
+        internal static Logger Logger { get; private set; } = null!;
+        internal static Plugin Instance { get; private set; } = null!;
 
         [Init]
-        public Plugin(IPA.Config.Config conf, Logger log)
+        public Plugin(Logger logger)
         {
             Instance = this;
-            Logger = log;
-            _harmony = new Harmony("bs.Exomanz.hsbr");
-            Logger.Debug("Config loaded");
+            Logger = logger;
+            Logger.Info("HitScoreBloomReviver initialized");
         }
 
         [OnEnable]
         public void Enable()
         {
             Logger.Debug("OnEnable");
-            _harmony.PatchAll(Assembly.GetExecutingAssembly());
+            _harmony.PatchAll();
         }
 
         [OnDisable]
         public void Disable()
         {
+            Logger.Debug("OnDisable");
             _harmony.UnpatchSelf();
-            _harmony = null;
         }
     }
 }
