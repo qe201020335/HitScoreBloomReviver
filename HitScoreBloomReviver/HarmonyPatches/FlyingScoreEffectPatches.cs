a2932b768a5e26555fc8fa7786b494c6b2bf9aef
qe201020335 <qe201020335@sina.com>
2025-04-04 23:37:18 -0400

    Copy bloom font creation logic from HitScoreVisualizer

--- HitScoreBloomReviver/HarmonyPatches/FlyingScoreEffectPatches.cs
+++ HitScoreBloomReviver/HarmonyPatches/FlyingScoreEffectPatches.cs
@@ -1,33 +1,86 @@
 ﻿using HarmonyLib;
-using IPA.Utilities;
 using System.Linq;
 using TMPro;
 using UnityEngine;
+using Object = UnityEngine.Object;
 
 namespace HitScoreBloomReviver.HarmonyPatches
 {
-    [HarmonyPatch(typeof(EffectPoolsManualInstaller), nameof(EffectPoolsManualInstaller.ManualInstallBindings), MethodType.Normal)]
+    [HarmonyPatch(typeof(EffectPoolsManualInstaller), nameof(EffectPoolsManualInstaller.ManualInstallBindings))]
     public static class FlyingScoreEffectMaterialPatch
     {
-        private static TMP_FontAsset customFontAsset;
+        private static TMP_FontAsset lazyBloomFont;
 
         [HarmonyPrefix]
         private static void Prefix(EffectPoolsManualInstaller __instance)
         {
+            lazyBloomFont ??= GetBloomFont();
+
+            if (lazyBloomFont == null)
+            {
+                Plugin.Logger.Warn("Bloom font not found, not updating hit score text.");
+                return;
+            }
+
             var text = __instance._flyingScoreEffectPrefab._text;
+            text.font = lazyBloomFont;
+        }
 
-            // I have to make a separate font since if I adjust the materials shader, everything 
-            // using the original FontAsset would have bloom applied to it.
-            customFontAsset = TMP_FontAsset.CreateFontAsset(Resources.FindObjectsOfTypeAll<TMP_FontAsset>().First(x => x.name.Contains(
-                "Teko-Medium SDF")).sourceFontFile);
-            customFontAsset.name = "Teko-Medium SDF Bloom";
+        /**
+         * Bloom font creation logic copied from HitScoreVisualizer.
+         */
+        private static TMP_FontAsset GetBloomFont()
+        {
+            var tekoFontAsset = Resources.FindObjectsOfTypeAll<TMP_FontAsset>()
+                .FirstOrDefault(x => x.name == "Teko-Medium SDF");
+            if (tekoFontAsset == null)
+            {
+                Plugin.Logger.Critical(
+                    "Teko-Medium SDF not found, unable to create bloom font. This is likely because of a game update.");
+                return null;
+            }
 
-            // Also can't use Mobile shaders since they lack the underlay that makes them glow.
-            // This results in a SubMesh applied to each FlyingScoreEffect object.
-            customFontAsset.material.shader = Resources.FindObjectsOfTypeAll<Shader>().First(x => x.name.Contains(
-                "TextMeshPro/Distance Field"));
+            var bloomFontShader = Resources.FindObjectsOfTypeAll<Shader>()
+                .FirstOrDefault(x => x.name == "TextMeshPro/Distance Field");
+            if (bloomFontShader == null)
+            {
+                Plugin.Logger.Critical(
+                    "Bloom font shader not found, unable to create bloom font. This is likely because of a game update.");
+                return null;
+            }
 
-            text.font = customFontAsset;
+            var bloomTekoFont = tekoFontAsset.CopyFontAsset("Teko-Medium SDF (Bloom)");
+            bloomTekoFont.material.shader = bloomFontShader;
+            return bloomTekoFont;
+        }
+
+        private static TMP_FontAsset CopyFontAsset(this TMP_FontAsset original, string newName)
+        {
+            if (string.IsNullOrEmpty(newName))
+            {
+                newName = original.name;
+            }
+
+            var newFontAsset = Object.Instantiate(original);
+
+            var texture = original.atlasTexture;
+
+            var newTexture = new Texture2D(texture.width, texture.height, texture.format, texture.mipmapCount, true)
+            {
+                name = $"{newName} Atlas"
+            };
+
+            Graphics.CopyTexture(texture, newTexture);
+
+            var material = new Material(original.material) { name = $"{newName} Atlas Material" };
+            material.SetTexture(Shader.PropertyToID("_MainTex"), newTexture);
+
+            newFontAsset.m_AtlasTexture = newTexture;
+            newFontAsset.name = newName;
+            newFontAsset.atlasTextures = new[] { newTexture };
+            newFontAsset.material = material;
+
+            return newFontAsset;
         }
     }
 }
